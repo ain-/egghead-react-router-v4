03f305d59a846f898026f2e24a2932bad0a91cac
Ain <ain.salula@gmail.com>
2017-03-17 20:39:59 +0200

    custom redirect and log in redirect

--- src/App12.js
+++ src/App12.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router,
-  Route, Link, Switch, Redirect } from 'react-router-dom';
+  Route, Link, Redirect } from 'react-router-dom';
 import './App.css';
 
+const loggedin = false;
 const Links = () => (
   <nav>
     <Link to="/">Home</Link>
-    <Link to="/old">Old</Link>
-    <Link to="/new">New</Link>
+    <Link to="/old/123">Old</Link>
+    <Link to="/new/456">New</Link>
+    <Link to="/protected">Protected</Link>
   </nav>
 );
 
@@ -16,9 +18,15 @@ const App = () => (
     <div>
       <Links />
       <Route exact path="/" render={() => (<h1>Home</h1>)} />
-      <Route path="/new" render={() => (<h1>New</h1>)}  />
-      <Route path="/old" render={() => (
-        <Redirect from="/old" to="/new" />
+      <Route path="/new/:str"
+        render={({match}) => (<h1>New: {match.params.str}</h1>)} />
+      <Route path="/old/:str" render={({match}) => (
+        <Redirect to={`/new/${match.params.str}`} />
+      )} />
+      <Route path="/protected" render={() => (
+        loggedin
+        ? <h1>Welcome to the protected page</h1>
+        :<Redirect to="new/Login" />
       )} />
     </div>
   </Router>
