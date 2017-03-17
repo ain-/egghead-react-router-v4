02265aff959bd0be0387c4fb0567bd35ea240d80
Ain <ain.salula@gmail.com>
2017-03-17 19:43:15 +0200

    nested routes

--- src/App11.js
+++ src/App11.js
@@ -4,11 +4,22 @@ import { BrowserRouter as Router,
 import './App.css';
 
 const Home = () => (<h1>Home</h1>);
-const Menu = () => (<h1>Menu</h1>);
+const Menu = () => (
+  <div>
+    <h1>Menu</h1>
+    <Link to="/menu/food">Food</Link>
+    <Link to="/menu/drink">Drinks</Link>
+    <Link to="/menu/sides">Sides</Link>
+    <Route path="/menu/:section"
+    render={({match}) => <h2>{match.params.section}</h2>} />
+  </div>
+);
 
 const App = () => (
   <Router>
     <div>
+      <Link to="/">Home</Link>
+      <Link to="/menu">Menu</Link>
       <Route exact path="/" component={Home} />
       <Route path="/menu" component={Menu} />
     </div>
