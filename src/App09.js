a7b13a0997200b58fc21959d90c1642b9128d6ad
Ain <ain.salula@gmail.com>
2017-03-17 16:27:05 +0200

    conditional route with switch

---
+++ src/App09.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter as Router,
+  Route, Link, Switch } from 'react-router-dom';
+import './App.css';
+
+const Links = () => (
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+    <Link to="/contact">Contact</Link>
+  </nav>
+);
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Links />
+          <Switch>
+            <Route exact path="/" render={() => <h1>Home</h1>} />
+            <Route path="/about" render={() => <h1>About</h1>} />
+            <Route path="/contact" render={() => <h1>Contact</h1>} />
+            <Route path="/:itemid"
+              render={({match}) => <h1>Item: {match.params.itemid}</h1>} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
