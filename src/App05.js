5ad40fc1a98a022e55468213812df73616cc31f2
Ain <ain.salula@gmail.com>
2017-03-17 14:38:37 +0200

    url params demos

---
+++ src/App05.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router,
+  Route } from 'react-router-dom';
+
+const App = () => (
+  <Router>
+    <div>
+      <Route path="/:page?-:subpage?" render={({match}) => (
+        <h1>
+          PAGE: {match.params.page || 'Home'}<br />
+          SUBPAGE: {match.params.subpage}
+        </h1>
+      )} />
+    </div>
+  </Router>
+);
+
+export default App;
