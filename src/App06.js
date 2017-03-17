c6d63603c47aee59807feeb751d54db592ed2136
Ain <ain.salula@gmail.com>
2017-03-17 15:13:29 +0200

    regex url matching

---
+++ src/App06.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router,
+  Route } from 'react-router-dom';
+
+const App = () => (
+  <Router>
+    <div>
+      <Route
+        path="/:a(\d{2}-\d{2}-\d{4})/:b(\.[a-z]+)" 
+        render={({match}) => (
+        <h1>
+          paramA: {match.params.a}<br />
+          paramB: {match.params.b}
+        </h1>
+      )} />
+    </div>
+  </Router>
+);
+
+export default App;
